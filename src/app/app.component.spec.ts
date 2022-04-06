9ca3ee0a44ba86b693f8fcd166bd1361c90c5f4c
Mathenge-Alex <arxmaina@gmail.com>
2022-04-06 17:04:25 +0300

    change app title reference to element reference app(['title']) in app.component.specs.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'Goals-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Goals-app');
+    expect(app['title']).toEqual('Goals-app');
   });
 
   it('should render title', () => {
