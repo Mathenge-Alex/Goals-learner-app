0be8bad13037a3c83490e8e7b63c9514b21ed65b
Mathenge-Alex <arxmaina@gmail.com>
2022-04-06 11:46:05 +0300

    create goal component and transferred the array of goals into it. Ensure Goal is imported.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goals-app';
+  // title = 'Goals-app';
+
+   // title = 'hello-world';
+  // goals: string;
+
+  // constructor(){
+  //   this.goals = ["Watch a movie", "See my Friend", "Sleep tight"]
+  // }
+
+
+  // 
+  // goals: Goal[] = [
+  //   {id:1, name: "Watch a movie",},
+  //   {id: 2, name: "See my Friend"},
+  //   {id: 3, name: "Sleep tight"},
+  //   {id: 4, name: "Compose a joke"},
+  //   {id: 5, name: "Eat a fruit on the swing"},
+  //   {id: 6, name: "Finish the project"}
+  // ]
 }
